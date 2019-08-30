0d75fad7ea04753359c21272ff8e9f72799ad915
tadashi <tadashi@pronetsoftware.com.br>
2019-08-30 01:37:53 -0300

    Inscrições feitas

--- InscricoesOnline/Controllers/HomeController.cs
+++ InscricoesOnline/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using InscricoesOnline.Models;
 using InscricoesOnline.Security;
+using InscricoesOnline.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -112,13 +114,21 @@ namespace InscricoesOnline.Controllers
         public ActionResult Equipe(Equipe equipe)
         {
             equipe.DataRegistro = DateTime.Now;
-            if (ModelState.IsValid)
-            {
-                equipe.Nome = equipe.Nome.ToUpper();
-                db.Equipes.Add(equipe);
-                db.SaveChanges();
 
-                return RedirectToAction("Inscricoes", new { id = equipe.Id });
+            var equipes = db.Equipes.Where(e => e.Email == equipe.Email).ToList();
+
+            if (equipes.Count > 0)
+                ViewBag.Error = "E-mail já está cadastrado no sistema";
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    equipe.Nome = equipe.Nome.ToUpper();
+                    db.Equipes.Add(equipe);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Inscricoes", new { id = equipe.Id });
+                }
             }
 
             return View(equipe);
@@ -139,7 +149,149 @@ namespace InscricoesOnline.Controllers
                 return HttpNotFound();
             }
 
-            return View(equipe);
+            ViewBag.Faixas = db.Faixas.OrderBy(f => f.Ordem).ToList();
+
+            var viewModelInscricoes = new ViewModelInscricoes
+            {
+                Equipe = equipe,
+                Inscricoes = db.Inscricoes.Where(i => i.AcademiaId == id).ToList()
+            };
+
+            return View(viewModelInscricoes);
+        }
+
+        public ActionResult BuscaCategoriaIdadeLuta(long ordem, string dataNascimento)
+        {
+            var idade = DateTime.Now.Year - Convert.ToDateTime(dataNascimento).Year;
+            var modalidadeId = ordem >= 10 ? 3 : 1;
+
+            var categoriaIdades = db.CategoriaIdades.Where(c => c.IdadeInicial <= idade && c.IdadeFinal >= idade && c.ModalidadeId == modalidadeId).OrderBy(c => c.IdadeInicial).ToList();
+
+            return Json(categoriaIdades);
+        }
+
+        public ActionResult BuscaCategoriaIdadePoomsae(string dataNascimento)
+        {
+            var idade = DateTime.Now.Year - Convert.ToDateTime(dataNascimento).Year;
+            var modalidadeId = 2;
+
+            var categoriaIdades = db.CategoriaIdades.Where(c => c.IdadeInicial <= idade && c.IdadeFinal >= idade && c.ModalidadeId == modalidadeId).OrderBy(c => c.IdadeInicial).ToList();
+
+            return Json(categoriaIdades);
+        }
+
+        public ActionResult BuscaCategoriaFaixaLuta(long ordem)
+        {
+            var modalidadeId = ordem >= 10 ? 3 : 1;
+
+            var categoriaFaixas = db.CategoriaFaixas.Where(c => c.FaixaInicial.Ordem <= ordem && c.FaixaFinal.Ordem >= ordem && c.ModalidadeId == modalidadeId).OrderBy(c => c.FaixaInicial.Ordem).ToList();
+
+            return Json(categoriaFaixas);
+        }
+
+        public ActionResult BuscaCategoriaFaixaPoomsae(long ordem)
+        {
+            var categoriaFaixas = db.CategoriaFaixas.Where(c => c.FaixaInicial.Ordem <= ordem && c.FaixaFinal.Ordem >= ordem && c.ModalidadeId == 2).OrderBy(c => c.FaixaInicial.Ordem).ToList();
+
+            return Json(categoriaFaixas);
+        }
+
+        public ActionResult BuscaCategoriaPesos(long categoriaIdadeId, string sexo)
+        {
+            var categoriaPesos = db.CategoriaLutaPeso.Where(c => c.CategoriaIdadeId == categoriaIdadeId && c.Sexo == sexo).OrderBy(c => c.PesoInicial).ToList();
+
+            return Json(categoriaPesos);
+        }
+
+        public ActionResult InserirAtleta(int eventoId, int equipeId, string nome, string nascimento, string sexo, int faixaId, bool luta, bool poomsae, bool paratkd,
+                                          int catFaixaLuta, int catIdadeLuta, int catPesoLuta,
+                                          int catFaixaPoomsae, int catIdadePoomsae)
+        {
+            var atleta = new Atleta()
+            {
+                Ativo = true,
+                Nome = nome,
+                DataNascimento = Convert.ToDateTime(nascimento),
+                DataRegistro = DateTime.Now,
+                EquipeId = equipeId,
+                FaixaId = faixaId,
+                Sexo = sexo,
+                Parataekwondo = paratkd
+            };
+
+            var inscricao = new Inscricao()
+            {
+                AcademiaId = equipeId,
+                Filiado = atleta,
+                DataInscricao = DateTime.Now,
+                EventoId = eventoId
+            };
+
+            db.Atletas.Add(atleta);
+            db.Inscricoes.Add(inscricao);
+
+            if (luta)
+            {
+                var modalidadeIdLuta = db.CategoriaIdades.Find(catIdadeLuta).ModalidadeId;
+
+                var inscricaoModalidade = new InscricaoModalidade()
+                {
+                    CategoriaFaixaId = catFaixaLuta,
+                    CategoriaIdadeId = catIdadeLuta,
+                    CategoriaLutaPesoId = catPesoLuta,
+                    EventoId = eventoId,
+                    Inscricao = inscricao,
+                    ModalidadeId = modalidadeIdLuta
+                };
+                db.InscricoesModalidades.Add(inscricaoModalidade);
+            }
+
+            if (poomsae)
+            {
+                var modalidadeIdPoomsae = db.CategoriaIdades.Find(catIdadePoomsae).ModalidadeId;
+
+                var inscricaoModalidade = new InscricaoModalidade()
+                {
+                    CategoriaFaixaId = catFaixaPoomsae,
+                    CategoriaIdadeId = catIdadePoomsae,
+                    EventoId = eventoId,
+                    Inscricao = inscricao,
+                    ModalidadeId = modalidadeIdPoomsae
+                };
+                db.InscricoesModalidades.Add(inscricaoModalidade);
+            }
+
+            db.SaveChanges();
+
+            var result = new
+            {
+                inscricao.Id
+            };
+
+            return Json(result);
+        }
+
+        public ActionResult DeleteInscricao(long id)
+        {
+            var inscricoes = db.Inscricoes.Find(id);
+
+            var inscricoesModalidades = inscricoes.InscricaoModalidade.Where(im => im.InscricaoId == inscricoes.Id).ToList();
+
+            for (var i = 0; i < inscricoesModalidades.Count(); i++)
+            {
+                db.InscricoesModalidades.Remove(inscricoesModalidades[i]);
+            }
+
+            db.Inscricoes.Remove(inscricoes);
+
+            db.SaveChanges();
+
+            var result = new
+            {
+                id
+            };
+
+            return Json(result);
         }
     }
 }
---
+++ InscricoesOnline/ViewModel/ViewModelInscricoes.cs
@@ -0,0 +1,14 @@
+﻿using InscricoesOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InscricoesOnline.ViewModel
+{
+    public class ViewModelInscricoes
+    {
+        public Equipe Equipe { get; set; }
+        public IEnumerable<Inscricao> Inscricoes { get; set; }
+    }
+}
